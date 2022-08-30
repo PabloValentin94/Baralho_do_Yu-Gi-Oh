54e67a0c61ea7cc493293a65dc5a97dea048ab05
PABLO VALENTIN <pablo.valentin@etec.sp.gov.br>
2022-08-30 15:27:13 -0300

    Editando o CSS.

--- App/DAO/MainPageDAO.php
+++ App/DAO/MainPageDAO.php
@@ -8,7 +8,7 @@ class MainPageDAO
     public function __construct()
     {
 
-        $dsn = "mysql:host=localhost:3306;dbname=db_yu-gi-oh";
+        $dsn = "mysql:host=localhost:3307;dbname=db_yu-gi-oh";
 
         $this->conexao = new PDO($dsn,"root","etecjau");
         
